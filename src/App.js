4817dc1c6fdba52c8f69cd45665cec02c685943d
Lexfer Ramirez <lexferramirez@gmail.com>
2021-09-26 09:40:40 -0400

    delete duplicated projectID props in ChatEngine component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = 'ff174dd1-33e6-463e-b86b-f6fef2cda410';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
@@ -15,7 +15,6 @@ const App = () => {
       projectID={projectID}
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
-      projectID='ff174dd1-33e6-463e-b86b-f6fef2cda410'
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
