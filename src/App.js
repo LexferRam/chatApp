1ec806193e388a2bfdc8eb7bfec45fa986070e3b
LisethEstadistica <liseth.estadistica@gmail.com>
2021-09-26 08:20:02 -0400

    create repo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import {ChatEngine} from 'react-chat-engine'
-import './App.css';
-import ChatFeed from './components/ChatFeed';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-      height='100vh'
-      userName='Lexfer'
-      userSecret='lfrp58942021'
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       projectID='ff174dd1-33e6-463e-b86b-f6fef2cda410'
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 //lfrp58942021
 export default App;
 
